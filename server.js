e1f26dc455b878cb0708ff6671b32acbb6c751d2
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-11-03 21:47:28 +0800

    added some comments

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => res.send("API running"));
+//what the user see when they access http://localhost:5000/
 
 const PORT = process.env.PORT || 5000;
 // if no environment port set, use 5000 (i.e for local use/default)
 
 app.listen(PORT, () => console.log(`Server created on ${PORT}`))
+//console message when server started
