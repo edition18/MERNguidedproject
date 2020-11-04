ee4890a4d9ccba87659d93bedfdcf5323c056d4a
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-11-04 09:14:29 +0800

    created user model

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String, required: true
+    },
+    email: {
+        type: String, required: true
+    },
+    password: {
+        type: String, required: true
+    },
+    avatar: {
+        type: String, required: true
+    },
+    date: {
+        type: Date, default: Date.now
+    } //note the use of the default property
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
