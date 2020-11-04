c83865594fc61b9b3a3a041531f1982567756255
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-11-04 21:44:25 +0800

    get profile by specific id route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -85,6 +85,44 @@ router.post("/",[auth,[
     }
 })
 
+// @route GET api/profile
+// @desc  GET all profiles
+// @access Public
 
+router.get("/", async (req,res) => {
+    try {
+        const profiles = await Profile.find().populate('user',['name','avatar']);
+        res.send(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("server error");
+    } 
+
+})
+
+// @route GET api/profile/user/user_id
+// @desc  GET profile by user id
+// @access Public
+
+router.get("/user/:user_id", async (req,res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user',['name','avatar']);
+        
+
+       if(!profile) {
+            return res.status(400).json({msg: "profile not found!"})
+        }
+
+        res.send(profile);
+
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind == "ObjectId") {
+            return res.status(400).json({msg: "profile not found!"})
+        }
+        res.status(500).send("server error");
+    } 
+
+})
 
 module.exports = router;
