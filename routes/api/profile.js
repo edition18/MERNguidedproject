57f9875f0af7ac23c4427e866a1eca1464f0863c
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-11-05 20:11:33 +0800

    add profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -132,11 +132,10 @@ router.get("/user/:user_id", async (req,res) => {
 
 router.delete("/", auth, async (req,res) => {
     try {
-        console.log(req);
         await Profile.findOneAndRemove({user: req.user.id});
         await User.findOneAndRemove({_id: req.user.id});
         // there is no id field in USer db, only the default _id
-        
+        // note that User id is copied onto Profile (field user) so they are the same
         res.json({msg: "user deleted"});
     } catch (err) {
         console.error(err.message);
@@ -144,5 +143,47 @@ router.delete("/", auth, async (req,res) => {
     } 
 
 })
+
+
+// @route PUT api/profile/experience (you could have POST as well)
+// @desc  add profile experience
+// @access Private
+
+router.put("/experience", [auth, [
+    check("title","title required").not().isEmpty(),
+    check("company","company required").not().isEmpty(),
+    check("from","from date required").not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+
+    const {title,company,location,from,to,current,description} = req.body;
+
+    const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    };
+
+    try {
+        const profile = await Profile.findOne({user: req.user.id })
+
+        profile.experience.unshift(newExp); //push new experience in front
+        await profile.save();
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
 
 module.exports = router;
