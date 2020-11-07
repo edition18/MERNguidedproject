505690b3cfe0ef3088b8a0d47696193d7cfb9029
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-11-07 13:22:22 +0800

    useState hooks on Register.js

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,72 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 
 const Register = () => {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: ""
+    // formData is the values, we have set them as default ""
+    //setFormData allows us to set the values
+    });
+
+    const {name,email,password,password2} = formData;
+    //destruct
+
+    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
+    // the brackets create a key
+    // this allows us to retain the current formData Object but changing only 1 state variable 
+    // .target is a html variable, it is the element that had triggered the event
+
+    const onSubmit = e => {
+        e.preventDefault(); //prevent default submit behavior
+        if(password !== password2) {
+            console.log("password not match");
+        } else {
+            console.log(formData);
+        }
+    }
+
     return (
-        <div>
-            Register
-        </div>
+        <Fragment>
+            <h1 className="large text-primary">Sign Up</h1>
+            <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
+                </div>
+                <div className="form-group">
+                <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required/>
+                <small className="form-text"
+                    >This site uses Gravatar so if you want a profile image, use a
+                    Gravatar email</small
+                >
+                </div>
+                <div className="form-group">
+                <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    value={password} onChange={e => onChange(e)} required
+                    minLength="6"
+                />
+                </div>
+                <div className="form-group">
+                <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    name="password2"
+                    value={password2} onChange={e => onChange(e)} required
+                    minLength="6"
+                />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Register" />
+            </form>
+            <p className="my-1">
+                Already have an account? <a href="login.html">Sign In</a>
+            </p>
+        </Fragment>
     )
 }
 
