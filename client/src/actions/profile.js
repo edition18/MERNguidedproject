45034350f189419418abce31c0e6b9143df9cf26
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-11-09 23:44:42 +0800

    Add Educationa dn Experiences

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,7 +3,7 @@
 import {setAlert} from "./alert";
 import axios from "axios";
 
-import {GET_PROFILE, PROFILE_ERROR} from "./types"
+import {GET_PROFILE, PROFILE_ERROR,UPDATE_PROFILE} from "./types"
 
 //GET current user profile
 export const getCurrentProfile = () => async dispatch => {
@@ -46,7 +46,7 @@ export const createProfile = (formData, history, edit = false) => async dispatch
             payload: res.data
         })
 
-        dispatch(setAlert(edit ? "Profile Updated" : "Profile Created"))
+        dispatch(setAlert(edit ? "Profile Updated" : "Profile Created", "success"))
 
         if(!edit) {
             history.push("/dashboard");
@@ -72,3 +72,80 @@ export const createProfile = (formData, history, edit = false) => async dispatch
     }
 }
 
+// add experience
+
+export const addExperience = (formData, history) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+
+        const res = await axios.put("/api/profile/experience", formData, config);
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        })
+
+        dispatch(setAlert("Experience Added", "sucess"))
+
+
+        history.push("/dashboard");
+
+    } catch (err) {
+        const errors = err.response.data.errors;
+        
+
+        if(errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg,"danger")));
+        } 
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status
+            }
+        })
+    }
+
+}
+
+export const addEducation = (formData, history) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+
+        const res = await axios.put("/api/profile/education", formData, config);
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        })
+
+        dispatch(setAlert("Education Added", "sucess"))
+
+
+        history.push("/dashboard");
+
+    } catch (err) {
+        const errors = err.response.data.errors;
+        
+
+        if(errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg,"danger")));
+        } 
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status
+            }
+        })
+    }
+
+}
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,6 +1,6 @@
 // root > client > src > reducers > profile.js
 
-import { PROFILE_ERROR, GET_PROFILE } from "../actions/types";
+import { PROFILE_ERROR, GET_PROFILE, UPDATE_PROFILE } from "../actions/types";
 
 const initialState = {
     profile: null, 
@@ -19,6 +19,7 @@ export default function(state=initialState, action){
     const {type, payload} = action;
 
     switch(type) {
+        case UPDATE_PROFILE:
         case GET_PROFILE:
             return {
                 ...state,
