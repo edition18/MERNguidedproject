c88fa8ccd14e2e629f2a317280d5e68d0bbfdc65
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-11-09 18:04:27 +0800

    createProfile action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -24,3 +24,51 @@ export const getCurrentProfile = () => async dispatch => {
         })
     }
 }
+
+// create or update profile
+
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+    // history object has a method called push
+    // will redirect us to a client side route
+    // edit boolean tells us if we are creating or editing a profile
+    // false by default
+
+    try {
+        const config = {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+
+        const res = await axios.post("/api/profile", formData, config);
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        })
+
+        dispatch(setAlert(edit ? "Profile Updated" : "Profile Created"))
+
+        if(!edit) {
+            history.push("/dashboard");
+        } 
+
+        // redirecting in an ACTION needs to use this history.push() method
+        // rather than the redirect in the HTML we used
+    } catch (err) {
+        const errors = err.response.data.errors;
+        
+
+        if(errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg,"danger")));
+        } 
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status
+            }
+        })
+    }
+}
+
