f6857b51743656fc69a79d7c0bc3cb0c2524241a
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-11-11 23:47:34 +0800

    starting on the profile view

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -267,7 +267,7 @@ export const deleteEducation = id => async dispatch => {
 export const deleteAccount = id => async dispatch => {
     if (window.confirm("Are you sure?")) { // confirm if u want to delete
         try {
-            const res = await axios.delete(`/api/profile`);
+            await axios.delete(`/api/profile`);
     
             dispatch({ type: CLEAR_PROFILE });
             dispatch({ type: ACCOUNT_DELETED });
--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -42,7 +42,7 @@ const EditProfile = ({ profile: {profile, loading},createProfile, history }) => 
             youtube: loading || !profile.youtube ? "" : profile.youtube,
             instagram: loading || !profile.instagram ? "" : profile.instagram,
         })
-    }, [loading]);
+    }, [loading, getCurrentProfile]);
 
     const {
         company,
