d063134f055a3dd58375294d652ef5779218945c
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-11-15 22:34:17 +0800

    Deployment ready

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
+const path = require();
 
 //connect to mongo
 connectDB();
@@ -8,8 +9,6 @@ connectDB();
 //init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send("API running"));
-//what the user see when they access http://localhost:5000/
 
 app.use("/api/users", require("./routes/api/users"))
 //any connection to /api/users will use routes in following file
@@ -17,6 +16,18 @@ app.use("/api/posts", require("./routes/api/posts"))
 app.use("/api/auth", require("./routes/api/auth"))
 app.use("/api/profile", require("./routes/api/profile"))
 
+// serve static assets in production
+if(process.env.NODE_ENV ==="production") {
+    // set static folder, being client/build folder
+    app.use(express.static("client/build"));
+
+
+    // serve the index.html file
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirname, "client" ,"build","index.html"));
+    })
+    // asterisk = anything apart from the api files here
+}
 
 const PORT = process.env.PORT || 5000;
 // if no environment port set, use 5000 (i.e for local use/default)
