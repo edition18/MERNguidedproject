77567b02e6353fc6f035076de26119a5f3917832
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-11-15 13:49:48 +0800

    PostItem com,ponent

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -5,7 +5,7 @@ import React, { Fragment, useEffect} from 'react';
 import {connect} from "react-redux";
 import {getPosts} from "../../actions/post";
 import Spinner from "../layout/Spinner";
-
+import PostItem from "./PostItem";
 
 
 const Posts = ({getPosts, post: {posts, loading}}) => {
@@ -15,9 +15,22 @@ const Posts = ({getPosts, post: {posts, loading}}) => {
     }, [getPosts]);
 
     return (
-        <div>
-            
-        </div>
+        loading ? <Spinner /> : (
+            <Fragment>
+                <h1 className="large text-primary">Post</h1>
+                <p className="lead">
+                    <i className="fas fa-user"></i> Welcome to the community
+                </p>
+                {/* Postform*/}
+                <div className="posts">
+                    {posts.map(post => (
+                        <PostItem key={post._id} post={post}/>
+                    ))}
+                </div>
+
+
+            </Fragment>
+        )
     )
 }
 
