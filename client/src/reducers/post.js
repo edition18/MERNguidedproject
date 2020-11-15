ad973fcea10969933f09c071584ccf82befd1381
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-11-15 15:09:28 +0800

    addLike and removeLike

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -2,7 +2,8 @@
 
 import {
     GET_POSTS,
-    POST_ERROR
+    POST_ERROR,
+    UPDATE_LIKES
 } from "../actions/types";
 
 const initialState = {
@@ -28,6 +29,15 @@ export default function(state = initialState,action) {
                 error: payload,
                 loading: false 
             };
+        case UPDATE_LIKES:
+            return {
+                ...state,
+                posts: state.posts.map(post => post._id === payload.id ? { ...post, likes: payload.likes } : post),
+                // for each post, check if they are the one we are on (by postId)
+                // if so, just manipulate the likes property
+                // if not map that post as it was
+                loading: false
+            }
         default:
             return state;
     }
