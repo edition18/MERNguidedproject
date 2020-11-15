5dad8e56f8e9e6ea2f657933d6e84a1e9fb9c2c5
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-11-15 22:11:50 +0800

    comment display and delete, PRE DEPLOYMENT

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -8,6 +8,7 @@ import Spinner from "../layout/Spinner";
 import { getPost } from "../../actions/post";
 import PostItem from "../posts/PostItem";
 import CommentForm from "../post/CommentForm";
+import CommentItem from "../post/CommentItem";
 
 
 const Post = ({getPost, post: {post, loading}, match})=> {
@@ -23,6 +24,11 @@ const Post = ({getPost, post: {post, loading}, match})=> {
         <Link to="/posts" className="btn">Back to posts</Link>
         <PostItem post={post} showActions={false} />
         <CommentForm postId={post._id} />
+        <div className="comments">
+            {post.comments.map(comment => (
+                <CommentItem key={comment._id} comment={comment} postId={post._id} />
+            ))}
+        </div>
     </Fragment>
 }
 
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -192,7 +192,7 @@ router.post('/comment/:id', [auth,[
 
         post.comments.unshift(newComment);
         await post.save();
-        res.json(post);
+        res.json(post.comments);
         
     } catch (err) {
         console.error(err.message);
