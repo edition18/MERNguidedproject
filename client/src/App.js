b0a19559f518fc5c4c6d618ba60d28ed1deca537
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-11-15 12:26:47 +0800

    set up Posts front end

--- client/src/App.js
+++ client/src/App.js
@@ -15,8 +15,8 @@ import AddExperience from "./components/profile-forms/AddExperience"
 import AddEducation from "./components/profile-forms/AddEducation"
 import Profiles from "./components/profiles/Profiles"
 import Profile from "./components/profile/Profile"
+import Posts from "./components/posts/Posts"
 import './App.css';
-// Redux
 import {Provider} from "react-redux";
 // redux is seperate from react
 // the provider can combine them together
@@ -58,6 +58,7 @@ return (
           <PrivateRoute exact path="/edit-profile" component={EditProfile} />
           <PrivateRoute exact path="/add-experience" component={AddExperience} />
           <PrivateRoute exact path="/add-education" component={AddEducation} />
+          <PrivateRoute exact path="/posts" component={Posts} />
         </Switch> 
       </section>
   </Fragment>
---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,33 @@
+//root > client > src > components > posts > Posts.js
+
+import PropTypes from 'prop-types'
+import React, { Fragment, useEffect} from 'react';
+import {connect} from "react-redux";
+import {getPosts} from "../../actions/post";
+import Spinner from "../layout/Spinner";
+
+
+
+const Posts = ({getPosts, post: {posts, loading}}) => {
+
+    useEffect(() => {
+        getPosts();
+    }, [getPosts]);
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Posts.propTypes = {
+    getPosts: PropTypes.func.isRequired,
+    post: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    post: state.post
+})
+
+export default connect(mapStateToProps, {getPosts})(Posts);
